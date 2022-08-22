fe5ee5988380cef81c159462607ca26322e2c9bf
ASHRAF1925 <1925ashraf@gmail.com>
2022-08-22 16:51:55 +0600

    basic functionalites done

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,9 @@ for (const button of buttons) {
 
 
         }
+        else {
+            alert("5 Players Already selected!")
+        }
 
 
     });
@@ -21,9 +24,46 @@ for (const button of buttons) {
 
 //Common Function to get input value
 function getInputValue(ElementId) {
-    // Selecting the input element and get its value 
-    var inputVal = document.getElementById(ElementId).value;
 
-    // Displaying the value
-    alert(inputVal);
+    const inputVal = document.getElementById(ElementId).value;
+    return inputVal
 }
+
+//validate function
+function inputvalidation(ElementId) {
+    document.getElementById(ElementId).addEventListener('keyup', function () {
+        console.log(document.getElementById(ElementId).value)
+        if (isNaN(document.getElementById(ElementId).value)) {
+            alert("Please Enter a Valid Number");
+            document.getElementById(ElementId).value = ""
+        }
+
+    })
+}
+inputvalidation('Player_budget');
+inputvalidation('Mangaer_cost');
+inputvalidation('Coach_cost');
+
+
+
+
+// calculate button
+const calculate_button = document.getElementById('calculate_button');
+const player_calculated_amount = document.getElementById('player_calculated_amount');
+calculate_button.addEventListener('click', function () {
+    console.log(calculate_button)
+    player_calculated_amount.innerText = getInputValue('Player_budget') * (playerList.childNodes.length - 1);
+
+});
+
+// Calculate Total Button
+const calculate_total_button = document.getElementById('calculate_total');
+
+calculate_total_button.addEventListener('click', function () {
+    console.log(document.getElementById('player_calculated_amount').innerText)
+    console.log(getInputValue('Mangaer_cost'))
+    console.log(getInputValue('Coach_cost'))
+    const total_cost = parseInt(getInputValue('Player_budget') * (playerList.childNodes.length - 1)) + parseInt(getInputValue('Mangaer_cost')) + parseInt(getInputValue('Coach_cost'));
+    document.getElementById('Total_cost').innerText = total_cost;
+
+});
