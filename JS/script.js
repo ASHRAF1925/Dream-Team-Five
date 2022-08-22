4f64105d98de00f24a384d17e330e93af9b25d71
ASHRAF1925 <1925ashraf@gmail.com>
2022-08-22 18:41:03 +0600

    basic validation done

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,12 @@
 const buttons = document.getElementsByClassName('selectButton');
 const playerList = document.getElementById('SelectedPlayerList');
+var per_player_cost_given = true;
+var manager_cost_given = true;
+var Coach_cost_given = true;
 //  Adding Selected Players to the list 
 for (const button of buttons) {
     button.addEventListener('click', function (event) {
+
         console.log(playerList.childNodes.length);
         if (playerList.childNodes.length < 6) {
             event.target.disabled = true;
@@ -44,6 +48,17 @@ inputvalidation('Player_budget');
 inputvalidation('Mangaer_cost');
 inputvalidation('Coach_cost');
 
+// empty validation
+function is_empty(ElementId) {
+
+    if (document.getElementById(ElementId).value == "") { return false; }
+    else {
+        return true;
+    }
+
+
+
+}
 
 
 
@@ -51,8 +66,18 @@ inputvalidation('Coach_cost');
 const calculate_button = document.getElementById('calculate_button');
 const player_calculated_amount = document.getElementById('player_calculated_amount');
 calculate_button.addEventListener('click', function () {
-    console.log(calculate_button)
-    player_calculated_amount.innerText = getInputValue('Player_budget') * (playerList.childNodes.length - 1);
+    per_player_cost_given = is_empty('Player_budget');
+    if ((playerList.childNodes.length - 1) <= 0) {
+        alert("No Players Selected !")
+    }
+
+    if (per_player_cost_given) {
+        player_calculated_amount.innerText = getInputValue('Player_budget') * (playerList.childNodes.length - 1);
+    }
+    else {
+        alert("Per Player Cost Input Field is Empty.Please Enter a number !")
+    }
+
 
 });
 
@@ -63,7 +88,29 @@ calculate_total_button.addEventListener('click', function () {
     console.log(document.getElementById('player_calculated_amount').innerText)
     console.log(getInputValue('Mangaer_cost'))
     console.log(getInputValue('Coach_cost'))
-    const total_cost = parseInt(getInputValue('Player_budget') * (playerList.childNodes.length - 1)) + parseInt(getInputValue('Mangaer_cost')) + parseInt(getInputValue('Coach_cost'));
-    document.getElementById('Total_cost').innerText = total_cost;
+    manager_cost_given = is_empty('Mangaer_cost');
+    Coach_cost_given = is_empty('Coach_cost');
+
+    if (manager_cost_given && Coach_cost_given) {
+        const total_cost = parseInt(getInputValue('Player_budget') * (playerList.childNodes.length - 1)) + parseInt(getInputValue('Mangaer_cost')) + parseInt(getInputValue('Coach_cost'));
+        document.getElementById('Total_cost').innerText = total_cost;
+    }
+    else {
+        if (manager_cost_given == false) {
+            alert(" Manager Cost Input Field is Empty.Please Enter a number !")
+
+        }
+        else if (Coach_cost_given == false) {
+            alert(" Coach Cost Input Field is Empty.Please Enter a number !")
+        }
+
+    }
+
+
+
+
+
+
+
 
 });
